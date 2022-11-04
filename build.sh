fc2cbb04b5ff1f0b47c562944e6bd4b7a5843bdc
sebastien-riou <sriou@nimp.co.uk>
2022-11-04 14:52:17 -0700

    better help message

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ mkdir -p build32
 cd build32
 ../configure  --enable-commitlog --with-isa=RV32IMAC --prefix=$RISCV
 make -j`nproc`
-echo "type install 'make install' to install to $RISCV"
+echo "type 'make -C `pwd` install' to install to $RISCV"

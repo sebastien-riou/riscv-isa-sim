a2814756358eb5698f3cd9d45f6dfd5d1ba14de8
sebastien-riou <sriou@nimp.co.uk>
2023-04-18 02:21:12 -0700

    sync

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build32
 cd build32
-../configure  --enable-commitlog --with-isa=RV32IMAC --prefix=$RISCV
+../configure  --with-isa=RV32IMAC --prefix=$RISCV
 make -j`nproc`
 echo "type 'make -C `pwd` install' to install to $RISCV"
